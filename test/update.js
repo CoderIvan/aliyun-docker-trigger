b490a120f8af14e21533b890f9dfd2d1fe98754a
XueHua Huang <xuehua.huang@dbjtech.com>
2019-07-11 17:01:14 +0800

    first init

--- test/update.js
+++ test/update.js
@@ -3,11 +3,11 @@ const runScript = require('../lib/run_script')
 describe('Exec', function root() {
 	this.timeout(60 * 1000)
 
-	it.skip('pullServiceImages', async () => {
-		await runScript.pullServiceImages('mercku-router-admin', 'test')
+	it('pullServiceImages', async () => {
+		await runScript.pullServiceImages('aliyun-docker-trigger', 'example')
 	})
 
-	it.skip('createAndStartContainers', async () => {
-		await runScript.createAndStartContainers('mercku-router-admin', 'test')
+	it('createAndStartContainers', async () => {
+		await runScript.createAndStartContainers('aliyun-docker-trigger', 'example')
 	})
 })
