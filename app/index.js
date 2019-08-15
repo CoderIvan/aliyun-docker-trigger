81294815527e1fe09ed17c84843031232ebe0964
XueHua Huang <xuehua.huang@dbjtech.com>
2019-08-15 16:59:03 +0800

    测试 1.0.1

--- app/index.js
+++ app/index.js
@@ -2,11 +2,14 @@ const http = require('http')
 
 const DEPLOY_TIME = new Date()
 
+const json = require('../package.json') // eslint-disable-line import/no-unresolved
+
 http.createServer((req, res) => {
 	res.end(`
     [Service]
     [Deploy] @ ${DEPLOY_TIME}
     [Request] @ ${new Date()}
+    [Version] @ ${json.version},
   `)
 }).listen(80, () => {
 	console.log('Server listening...') // eslint-disable-line no-console
