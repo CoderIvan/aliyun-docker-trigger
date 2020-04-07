46b1ca3d9b131e83efd1a5d801855497055d35e4
XueHua Huang <xuehua.huang@dbjtech.com>
2020-04-07 10:32:27 +0800

    修复日志模块错误

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const Server = require('./lib/app')
 const getDockerConfig = require('./lib/get_docker_config')
 const runScript = require('./lib/run_script')
+const myConsole = require('./lib/console')
 
 const config = require('./config')
 
 const server = new Server()
 
 server.listen(config.port).then(() => {
-	console.log(`Server Listen @ ${config.port}`)
+	myConsole.log(`Server Listen @ ${config.port}`)
 })
 
 getDockerConfig()
-	.then(files => Promise.all(files.map(async ({ name, tag }) => {
-		console.log(`create container ${name}/${tag}`)
+	.then((files) => Promise.all(files.map(async ({ name, tag }) => {
+		myConsole.log(`create container ${name}/${tag}`)
 		try {
-			await runScript.createOrUpdateContainers(name, tag)
-			console.log(`create container ${name}/${tag} finish`)
+			await runScript(myConsole).createOrUpdateContainers(name, tag)
+			myConsole.log(`create container ${name}/${tag} finish`)
 		} catch (error) {
-			console.error(`create container ${name}/${tag} error`)
-			console.error(error)
+			myConsole.error(`create container ${name}/${tag} error`)
+			myConsole.error(error)
 		}
 	})))
